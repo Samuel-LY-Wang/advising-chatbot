00a67fe5c785335ca3eee3f5c348d18736905131
Samuel-LY-Wang <samuellywang@gmail.com>
2026-05-10 10:26:24 -0400

    Moved headers to config as well

--- pipelines/bulk_sources_crawler.py
+++ pipelines/bulk_sources_crawler.py
@@ -23,11 +23,7 @@ MAPPING_FILE = os.path.join(MAPPING_PATH, config["doc_mapping_file"])
 strip=[5,9]
 FETCH_ERROR_FILE = os.path.join(config["cwd"], config["HTTP_fetch_error_file"])
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/120.0.0.0 Safari/537.36"
-}
+HEADERS = config["header"]
 
 SOURCES = config["sources"]
 
