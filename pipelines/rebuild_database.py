5ac09684f5de0e78b01f0caf3119531db01c9c9e
Samuel-LY-Wang <samuellywang@gmail.com>
2026-05-25 11:55:09 -0400

    Changed embed model because the previous one caused issues on Nvidia GPUs

--- pipelines/rebuild_database.py
+++ pipelines/rebuild_database.py
@@ -15,18 +15,18 @@ import logging
 logging.basicConfig(level=logging.INFO, filename="logs/rebuild_db.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-def main(verbose=False):
+def main(verbose=False, batch_size=100):
     if verbose:
         logger.info("Starting database rebuild...")
         time_execution(fetch_all, out="Webscraping time: ", logger=logger)
         time_execution(generate_data_store, out="Chunk generation time: ", logger=logger)
         chunks = time_execution(load_chunks, out="Chunk loading time: ", logger=logger)
-        time_execution(lambda: save_to_chroma(chunks), out="DB saving time: ", logger=logger)
+        time_execution(lambda: save_to_chroma(chunks, batch_size=batch_size), out="DB saving time: ", logger=logger)
     else:
         fetch_all()
         generate_data_store()
         chunks = load_chunks()
-        save_to_chroma(chunks)
+        save_to_chroma(chunks, batch_size=batch_size)
 
 if __name__ == "__main__":
-    main(verbose=True)
+    main(verbose=True, batch_size=100)
